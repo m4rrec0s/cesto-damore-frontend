26fad22c37b614b4448700c20f6d18d8d639bedc
Marcos Araujo <mine.rique.games@gmail.com>
2026-07-08 22:34:20 -0300

    feat: add Google Tag Manager integration to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./animations.css";
+import Script from "next/script";
 import { AuthProvider } from "./hooks/use-auth";
 import ClientLayout from "./components/layout/client-layout";
 import { Toaster } from "./components/ui/sonner";
@@ -31,12 +32,37 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" data-scroll-behavior="smooth">
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-52R2V6C8');
+        `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link rel="dns-prefetch" href={apiOrigin} />
         <link rel="preconnect" href={apiOrigin} crossOrigin="" />
         <link rel="dns-prefetch" href="https://drive.google.com" />
         <link rel="preconnect" href="https://drive.google.com" crossOrigin="" />
       </head>
       <body className="antialiased bg-gray-50 text-gray-900 font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-52R2V6C8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {MAINTENANCE_MODE_ENABLED ? (
           <main className="min-h-screen bg-gray-950 text-white flex items-center justify-center px-6">
             <section className="w-full max-w-2xl rounded-2xl border border-white/15 bg-white/5 p-8 text-center">
